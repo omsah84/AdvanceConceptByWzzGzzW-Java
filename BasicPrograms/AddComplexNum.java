878403a1f93e8360f640bd23f5cc8537d8ce0df7
omsah <huncyoms@gmail.com>
2023-04-23 09:42:49 -0400

    commit

--- BasicPrograms/AddComplexNum.java
+++ BasicPrograms/AddComplexNum.java
@@ -1,6 +1,8 @@
 public class AddComplexNum {
+    //instance variable 
     int real, image;
 
+    //constructor whick make real and image part 
     AddComplexNum(int r, int i) {
         this.real = r;
         this.image = i;
@@ -8,11 +10,12 @@ public class AddComplexNum {
 
 
 
+    //complext value display
     public void show() {
         System.out.println(this.real + " i" + this.image);
     }
     
-    
+    //adding two complex number
     public static AddComplexNum sum(AddComplexNum c1, AddComplexNum c2) {
         AddComplexNum res = new AddComplexNum(0, 0);
 
@@ -24,16 +27,22 @@ public class AddComplexNum {
     }
 
 
+    //driver code
     public static void main(String[] args) {
+        //two complext number
         AddComplexNum c1 = new AddComplexNum(2, 2);
         AddComplexNum c2 = new AddComplexNum(2, 2);
 
+        //printing first complex number
         c1.show();
-
+        
+        //printing first complex number
         c2.show();
 
+        //add function
         AddComplexNum res = sum(c1, c2);
         
+        //display result
         System.out.println("The addtion: ");
         res.show();
 
