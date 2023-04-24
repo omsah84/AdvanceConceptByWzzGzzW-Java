cad5eee59ac0e6dfd057c792fcbfba5b77604cf9
omsah <huncyoms@gmail.com>
2023-04-24 01:46:14 -0400

    commit

---
+++ BasicPrograms/VowOrConso.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+public class VowOrConso {
+
+    //to check given character is vowel or consonant.
+
+    static String voString(char c) {
+
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O'
+                || c == 'U') {
+            return "vowel";
+        }
+
+        else {
+            return "consonant";
+        }
+
+    }
+    
+    //dive code
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        
+        //taking char value
+        System.out.println("Enter the character...");
+        char c = bf.readLine().charAt(0);
+
+        //ret
+        String str = voString(c);
+    
+        System.out.println(str == "vowel"? "vowel":"consonant");
+    }
+}
