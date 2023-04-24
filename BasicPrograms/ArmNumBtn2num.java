16bade9183e4f4a63a9f2e33405b19eb29839d1d
omsah <huncyoms@gmail.com>
2023-04-24 03:21:27 -0400

    commit

--- BasicPrograms/ArmNumBtn2num.java
+++ BasicPrograms/ArmNumBtn2num.java
@@ -2,8 +2,11 @@ import java.util.*;
 import java.io.*;
 import java.lang.*;
 
+//java program to find armstrong
+//number between two number 
 public class ArmNumBtn2num {
-
+    
+    //to find the armstrong number 
     static void armStrongNum(int num1, int num2) {
         if (num1 > 0 && num2 > 1) {
             for (int i = num1 + 1; i < num2; i++) {
@@ -11,10 +14,9 @@ public class ArmNumBtn2num {
                 if (sum == i) {
                     show(sum);
                 }
-
             }
         }
-        
+
         else {
             System.out.println("Enter greater than 0,1...");
         }
@@ -22,33 +24,35 @@ public class ArmNumBtn2num {
         return;
     }
     
+    //check armstrong number or not and return it
     static int findarm(int i) {
-        // int originalNum = i;
         int sum = 0;
 
         while (i != 0) {
             int rem = i % 10;
+
+            //cube function
             int cube = cub(rem);
-
             sum = sum + cube;
-
             i = i / 10;
         }
 
         return sum;
-
     }
 
+
+    //finding cube of number 
     static int cub(int rem) {
-        return rem*rem*rem;
-        
+        return rem * rem * rem;
     }
     
+    //print armstrong number
     static void show(int num) {
-        System.out.print(num+" ");
+        System.out.print(num + " ");
         return;
     }
-
+    
+    //drive code..
     public static void main(String[] args) throws Exception {
 
         while (true) {
@@ -61,6 +65,8 @@ public class ArmNumBtn2num {
             int num2 = Integer.parseInt(bf.readLine());
 
             System.out.print("The armstrong number is: ");
+
+            //calling armstrong finder function
             armStrongNum(num1, num2);
 
             System.out.println("\n===================");
