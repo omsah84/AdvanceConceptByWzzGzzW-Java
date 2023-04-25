8199c8457f9e9e485a994e8e9af49c321a6f9b2a
omsah <huncyoms@gmail.com>
2023-04-25 12:46:02 -0400

    commit

--- BasicPrograms/AllPrimeNum.java
+++ BasicPrograms/AllPrimeNum.java
@@ -2,9 +2,24 @@ public class AllPrimeNum {
     public static void main(String[] args) {
         int N = 45;
         prime_N(N);
+
     }
 
     private static void prime_N(int n) {
-        int x, y, flag;
+        int count = 0;
+        for (int i = 2; i <= n; i++) {
+            if (i % i == 0 && i%1==0) {
+                count++;
+            }
+
+            if (count == 2) {
+                show(i);
+            }
+        }
+    }
+
+    static void show(int i) {
+        System.out.println(i);
+        return;
     }
 }
