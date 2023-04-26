35e4f65005155cc1760179dd7450f24d0a35b309
omsah <huncyoms@gmail.com>
2023-04-26 13:07:30 -0400

    commit

--- BasicPrograms/Fac.java
+++ BasicPrograms/Fac.java
@@ -2,20 +2,21 @@ import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Fac {
+    //factorial function
     static void fac(int num) {
         int sum = 1;
-        if (num == 0 || num==1) {
+        if (num == 0 || num == 1) {
             System.out.println("The factorial is : 1");
-        }
-        else 
-        {
+        } else {
             for (int i = 2; i <= num; i++) {
                 sum = sum * i;
             }
-            System.out.println("The factorial is : "+sum);
+            System.out.println("The factorial is : " + sum);
 
         }
     }
+    
+    //driver code
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         while (true) {
