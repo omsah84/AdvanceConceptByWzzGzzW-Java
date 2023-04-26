e3bb751d45ff568f67df11c881f7b8f0647a24fd
omsah <huncyoms@gmail.com>
2023-04-26 12:19:00 -0400

    commit

--- BasicPrograms/AllPrimeNum.java
+++ BasicPrograms/AllPrimeNum.java
@@ -6,12 +6,13 @@ public class AllPrimeNum {
     }
 
     private static void prime_N(int n) {
-        int count = 0;
         for (int i = 2; i <= n; i++) {
-            if (i % i == 0 && i%1==0) {
-                count++;
+            int count = 0;
+            for (int j = 1; j <= n; j++) {
+                if (i % j == 0) {
+                    count++;
+                }      
             }
-
             if (count == 2) {
                 show(i);
             }
@@ -19,7 +20,7 @@ public class AllPrimeNum {
     }
 
     static void show(int i) {
-        System.out.println(i);
+        System.out.print(i+" ");
         return;
     }
 }
