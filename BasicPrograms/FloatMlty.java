98291403a4f0e5afb307b43a3fab3087bc521b8e
omsah <huncyoms@gmail.com>
2023-04-26 13:09:49 -0400

    commit

--- BasicPrograms/FloatMlty.java
+++ BasicPrograms/FloatMlty.java
@@ -5,23 +5,23 @@ import java.io.*;
 
 //main class
 public class FloatMlty {
-        static class FunctionCode {
+    //static class for the calculate the floating multiplication
+    static class FunctionCode {
         float f1;
         float f2;
-        
-        FunctionCode(float f1, float f2) throws Exception{
+
+        //constructor
+        FunctionCode(float f1, float f2) throws Exception {
             this.f1 = f1;
             this.f2 = f2;
-            
-            System.out.println("The floating value multiplication is: "+f1*f2);
+
+            System.out.println("The floating value multiplication is: " + f1 * f2);
         }
-
-
     }
+    //dive code..
     public static void main(String[] args) throws Exception{
         //creating object  of bufferedReader class
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
 
         //taking floating data from user
         //first floating data and converting as a float
@@ -31,8 +31,6 @@ public class FloatMlty {
         //second floating data and converting as a float
         System.out.println("Enter the floating value.");
         float f2 = Float.parseFloat(bf.readLine());
-
-
         //object of function class
         FunctionCode ohh = new FunctionCode(f1, f2);
         // FunctionCode oh = new FunctionCode();
