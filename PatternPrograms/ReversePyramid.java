1b9dbb367c0e8a702ed854aaad21cf93fd233e64
omsah <huncyoms@gmail.com>
2023-04-26 14:32:39 -0400

    commit

--- PatternPrograms/ReversePyramid.java
+++ PatternPrograms/ReversePyramid.java
@@ -1,18 +1,18 @@
 package PatternPrograms;
 
-public class LeftTriangle {
+import javax.swing.plaf.basic.BasicBorders.MarginBorder;
+
+public class ReversePyramid {
     public static void main(String[] args) {
         int n = 5;
-        for (int i = 1; i <= n; i++) {
-
+        for (int i = n; i >= 1; i--) {
             for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+            for(int j = i ;j>=1;j--){
+                System.out.print("* ");
             }
             System.out.println();
         }
     }
-    
 }
