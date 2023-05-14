42af079f0ff7f4d940a970c47aae7c9e40da63ec
omsah <huncyoms@gmail.com>
2023-05-14 08:45:23 -0400

    commit

--- Stack/StackClass.java
+++ Stack/StackClass.java
@@ -20,8 +20,7 @@ public class StackClass {
 
         static Node head;
 
-
-        
+        //isempty function
         public static boolean isEmpty() {
             return head == null;
         }
